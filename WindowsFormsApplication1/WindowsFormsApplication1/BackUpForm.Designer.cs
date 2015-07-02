d5df965a691a98ead8e637103bddb4dc2b77e745
IGA <ds934118@gmail.com>
2015-07-02 19:45:07 +0800

    UI
    UI & NotifyIcon

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/BackUpForm.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace WindowsFormsApplication1 {
+    partial class BackUpForm {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BackUpForm));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.main_button_RecoverData = new System.Windows.Forms.Button();
+            this.main_button_BackUpSetting = new System.Windows.Forms.Button();
+            this.main_panel_BackUpSetting = new System.Windows.Forms.Panel();
+            this.main_comboBox_weekly = new System.Windows.Forms.ComboBox();
+            this.main_comboBox_monthly = new System.Windows.Forms.ComboBox();
+            this.main_dateTimePicker_daily = new System.Windows.Forms.DateTimePicker();
+            this.main_radioButton_unused = new System.Windows.Forms.RadioButton();
+            this.main_radioButton_monthly = new System.Windows.Forms.RadioButton();
+            this.main_radioButton_weekly = new System.Windows.Forms.RadioButton();
+            this.main_radioButton_daily = new System.Windows.Forms.RadioButton();
+            this.main_panel_RecoverData = new System.Windows.Forms.Panel();
+            this.Icon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.IconStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.結束ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.main_button_Exit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.main_panel_BackUpSetting.SuspendLayout();
+            this.IconStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.main_button_Exit);
+            this.splitContainer1.Panel1.Controls.Add(this.main_button_RecoverData);
+            this.splitContainer1.Panel1.Controls.Add(this.main_button_BackUpSetting);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.main_panel_BackUpSetting);
+            this.splitContainer1.Panel2.Controls.Add(this.main_panel_RecoverData);
+            this.splitContainer1.Size = new System.Drawing.Size(664, 381);
+            this.splitContainer1.SplitterDistance = 148;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // main_button_RecoverData
+            // 
+            this.main_button_RecoverData.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.main_button_RecoverData.Location = new System.Drawing.Point(20, 164);
+            this.main_button_RecoverData.Margin = new System.Windows.Forms.Padding(2);
+            this.main_button_RecoverData.Name = "main_button_RecoverData";
+            this.main_button_RecoverData.Size = new System.Drawing.Size(104, 44);
+            this.main_button_RecoverData.TabIndex = 1;
+            this.main_button_RecoverData.Text = "還原檔案";
+            this.main_button_RecoverData.UseVisualStyleBackColor = true;
+            this.main_button_RecoverData.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // main_button_BackUpSetting
+            // 
+            this.main_button_BackUpSetting.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.main_button_BackUpSetting.Location = new System.Drawing.Point(20, 22);
+            this.main_button_BackUpSetting.Margin = new System.Windows.Forms.Padding(2);
+            this.main_button_BackUpSetting.Name = "main_button_BackUpSetting";
+            this.main_button_BackUpSetting.Size = new System.Drawing.Size(104, 44);
+            this.main_button_BackUpSetting.TabIndex = 0;
+            this.main_button_BackUpSetting.Text = "備份設定";
+            this.main_button_BackUpSetting.UseVisualStyleBackColor = true;
+            this.main_button_BackUpSetting.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // main_panel_BackUpSetting
+            // 
+            this.main_panel_BackUpSetting.Controls.Add(this.main_comboBox_weekly);
+            this.main_panel_BackUpSetting.Controls.Add(this.main_comboBox_monthly);
+            this.main_panel_BackUpSetting.Controls.Add(this.main_dateTimePicker_daily);
+            this.main_panel_BackUpSetting.Controls.Add(this.main_radioButton_unused);
+            this.main_panel_BackUpSetting.Controls.Add(this.main_radioButton_monthly);
+            this.main_panel_BackUpSetting.Controls.Add(this.main_radioButton_weekly);
+            this.main_panel_BackUpSetting.Controls.Add(this.main_radioButton_daily);
+            this.main_panel_BackUpSetting.Location = new System.Drawing.Point(-1, 0);
+            this.main_panel_BackUpSetting.Margin = new System.Windows.Forms.Padding(2);
+            this.main_panel_BackUpSetting.Name = "main_panel_BackUpSetting";
+            this.main_panel_BackUpSetting.Size = new System.Drawing.Size(514, 378);
+            this.main_panel_BackUpSetting.TabIndex = 0;
+            // 
+            // main_comboBox_weekly
+            // 
+            this.main_comboBox_weekly.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.main_comboBox_weekly.Enabled = false;
+            this.main_comboBox_weekly.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.main_comboBox_weekly.FormattingEnabled = true;
+            this.main_comboBox_weekly.Items.AddRange(new object[] {
+            "一",
+            "二",
+            "三",
+            "四",
+            "五",
+            "六",
+            "日"});
+            this.main_comboBox_weekly.Location = new System.Drawing.Point(209, 71);
+            this.main_comboBox_weekly.Margin = new System.Windows.Forms.Padding(2);
+            this.main_comboBox_weekly.Name = "main_comboBox_weekly";
+            this.main_comboBox_weekly.Size = new System.Drawing.Size(92, 26);
+            this.main_comboBox_weekly.TabIndex = 5;
+            // 
+            // main_comboBox_monthly
+            // 
+            this.main_comboBox_monthly.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.main_comboBox_monthly.Enabled = false;
+            this.main_comboBox_monthly.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.main_comboBox_monthly.FormattingEnabled = true;
+            this.main_comboBox_monthly.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31"});
+            this.main_comboBox_monthly.Location = new System.Drawing.Point(209, 117);
+            this.main_comboBox_monthly.Margin = new System.Windows.Forms.Padding(2);
+            this.main_comboBox_monthly.Name = "main_comboBox_monthly";
+            this.main_comboBox_monthly.Size = new System.Drawing.Size(92, 26);
+            this.main_comboBox_monthly.TabIndex = 4;
+            // 
+            // main_dateTimePicker_daily
+            // 
+            this.main_dateTimePicker_daily.CustomFormat = " HH 點 mm 分";
+            this.main_dateTimePicker_daily.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.main_dateTimePicker_daily.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.main_dateTimePicker_daily.Location = new System.Drawing.Point(192, 25);
+            this.main_dateTimePicker_daily.Margin = new System.Windows.Forms.Padding(2);
+            this.main_dateTimePicker_daily.Name = "main_dateTimePicker_daily";
+            this.main_dateTimePicker_daily.ShowUpDown = true;
+            this.main_dateTimePicker_daily.Size = new System.Drawing.Size(109, 30);
+            this.main_dateTimePicker_daily.TabIndex = 3;
+            this.main_dateTimePicker_daily.Value = new System.DateTime(2015, 8, 6, 0, 0, 0, 0);
+            // 
+            // main_radioButton_unused
+            // 
+            this.main_radioButton_unused.AutoSize = true;
+            this.main_radioButton_unused.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.main_radioButton_unused.Location = new System.Drawing.Point(33, 164);
+            this.main_radioButton_unused.Margin = new System.Windows.Forms.Padding(2);
+            this.main_radioButton_unused.Name = "main_radioButton_unused";
+            this.main_radioButton_unused.Size = new System.Drawing.Size(84, 23);
+            this.main_radioButton_unused.TabIndex = 6;
+            this.main_radioButton_unused.TabStop = true;
+            this.main_radioButton_unused.Text = "不使用";
+            this.main_radioButton_unused.UseVisualStyleBackColor = true;
+            // 
+            // main_radioButton_monthly
+            // 
+            this.main_radioButton_monthly.AutoSize = true;
+            this.main_radioButton_monthly.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.main_radioButton_monthly.Location = new System.Drawing.Point(33, 119);
+            this.main_radioButton_monthly.Margin = new System.Windows.Forms.Padding(2);
+            this.main_radioButton_monthly.Name = "main_radioButton_monthly";
+            this.main_radioButton_monthly.Size = new System.Drawing.Size(65, 23);
+            this.main_radioButton_monthly.TabIndex = 2;
+            this.main_radioButton_monthly.TabStop = true;
+            this.main_radioButton_monthly.Text = "每月";
+            this.main_radioButton_monthly.UseVisualStyleBackColor = true;
+            this.main_radioButton_monthly.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // main_radioButton_weekly
+            // 
+            this.main_radioButton_weekly.AutoSize = true;
+            this.main_radioButton_weekly.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.main_radioButton_weekly.Location = new System.Drawing.Point(33, 74);
+            this.main_radioButton_weekly.Margin = new System.Windows.Forms.Padding(2);
+            this.main_radioButton_weekly.Name = "main_radioButton_weekly";
+            this.main_radioButton_weekly.Size = new System.Drawing.Size(65, 23);
+            this.main_radioButton_weekly.TabIndex = 1;
+            this.main_radioButton_weekly.TabStop = true;
+            this.main_radioButton_weekly.Text = "每週";
+            this.main_radioButton_weekly.UseVisualStyleBackColor = true;
+            this.main_radioButton_weekly.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // main_radioButton_daily
+            // 
+            this.main_radioButton_daily.AutoSize = true;
+            this.main_radioButton_daily.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.main_radioButton_daily.Location = new System.Drawing.Point(33, 30);
+            this.main_radioButton_daily.Margin = new System.Windows.Forms.Padding(2);
+            this.main_radioButton_daily.Name = "main_radioButton_daily";
+            this.main_radioButton_daily.Size = new System.Drawing.Size(65, 23);
+            this.main_radioButton_daily.TabIndex = 0;
+            this.main_radioButton_daily.TabStop = true;
+            this.main_radioButton_daily.Text = "每日";
+            this.main_radioButton_daily.UseVisualStyleBackColor = true;
+            this.main_radioButton_daily.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // main_panel_RecoverData
+            // 
+            this.main_panel_RecoverData.Location = new System.Drawing.Point(-1, 0);
+            this.main_panel_RecoverData.Margin = new System.Windows.Forms.Padding(2);
+            this.main_panel_RecoverData.Name = "main_panel_RecoverData";
+            this.main_panel_RecoverData.Size = new System.Drawing.Size(514, 378);
+            this.main_panel_RecoverData.TabIndex = 1;
+            // 
+            // Icon
+            // 
+            this.Icon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.Icon.BalloonTipText = "程式並未結束。";
+            this.Icon.BalloonTipTitle = "BackUp常駐中";
+            this.Icon.ContextMenuStrip = this.IconStrip;
+            this.Icon.Icon = ((System.Drawing.Icon)(resources.GetObject("Icon.Icon")));
+            this.Icon.Text = "BackUp程式";
+            this.Icon.Visible = true;
+            this.Icon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // IconStrip
+            // 
+            this.IconStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.結束ToolStripMenuItem});
+            this.IconStrip.Name = "IconStrip";
+            this.IconStrip.Size = new System.Drawing.Size(123, 26);
+            // 
+            // 結束ToolStripMenuItem
+            // 
+            this.結束ToolStripMenuItem.Name = "結束ToolStripMenuItem";
+            this.結束ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.結束ToolStripMenuItem.Text = "結束程式";
+            this.結束ToolStripMenuItem.Click += new System.EventHandler(this.結束ToolStripMenuItem_Click);
+            // 
+            // main_button_Exit
+            // 
+            this.main_button_Exit.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.main_button_Exit.Location = new System.Drawing.Point(20, 303);
+            this.main_button_Exit.Margin = new System.Windows.Forms.Padding(2);
+            this.main_button_Exit.Name = "main_button_Exit";
+            this.main_button_Exit.Size = new System.Drawing.Size(104, 44);
+            this.main_button_Exit.TabIndex = 2;
+            this.main_button_Exit.Text = "關閉程式";
+            this.main_button_Exit.UseVisualStyleBackColor = true;
+            this.main_button_Exit.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // BackUpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 381);
+            this.Controls.Add(this.splitContainer1);
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "BackUpForm";
+            this.Text = "BackUp";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.BackUpForm_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.main_panel_BackUpSetting.ResumeLayout(false);
+            this.main_panel_BackUpSetting.PerformLayout();
+            this.IconStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button main_button_RecoverData;
+        private System.Windows.Forms.Button main_button_BackUpSetting;
+        private System.Windows.Forms.Panel main_panel_RecoverData;
+        private System.Windows.Forms.Panel main_panel_BackUpSetting;
+        private System.Windows.Forms.RadioButton main_radioButton_daily;
+        private System.Windows.Forms.RadioButton main_radioButton_monthly;
+        private System.Windows.Forms.RadioButton main_radioButton_weekly;
+        private System.Windows.Forms.DateTimePicker main_dateTimePicker_daily;
+        private System.Windows.Forms.ComboBox main_comboBox_monthly;
+        private System.Windows.Forms.ComboBox main_comboBox_weekly;
+        private System.Windows.Forms.RadioButton main_radioButton_unused;
+        private System.Windows.Forms.NotifyIcon Icon;
+        private System.Windows.Forms.ContextMenuStrip IconStrip;
+        private System.Windows.Forms.ToolStripMenuItem 結束ToolStripMenuItem;
+        private System.Windows.Forms.Button main_button_Exit;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/BackUpForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1 {
+    public partial class BackUpForm : Form {
+        public BackUpForm() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            main_panel_BackUpSetting.Show();
+            main_panel_RecoverData.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            main_panel_BackUpSetting.Hide();
+            main_panel_RecoverData.Show();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e) {
+            main_comboBox_monthly.Enabled = false;
+            main_comboBox_weekly.Enabled = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e) {
+            main_comboBox_weekly.Enabled = true;
+            main_comboBox_monthly.Enabled = false;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e) {
+            main_comboBox_weekly.Enabled = false;
+            main_comboBox_monthly.Enabled = true;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            e.Cancel = true;
+            this.Hide();
+            Icon.ShowBalloonTip(3000);
+        }
+
+        private void 結束ToolStripMenuItem_Click(object sender, EventArgs e) {
+            Environment.Exit(Environment.ExitCode);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e) {
+            Environment.Exit(Environment.ExitCode);
+        }
+
+        private void BackUpForm_Load(object sender, EventArgs e) {
+
+        }
+
+    }
+}
